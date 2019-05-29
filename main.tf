0c8f3517bfc27fca01296700c97c7d5b95967ad1
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2019-05-29 03:46:09 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "3tier" {
+  source  = "app.terraform.io/arslanhashi/3tier/azure"
+  version = "1.0.1"
+
+  prefix = "AZLMODULE"
+}
